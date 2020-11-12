daf7c26d6ea09d93c280caded30cfe65c1f5ded6
mrlucca <vini.lucca42@gmail.com>
2020-11-12 20:44:39 -0300

    New implementation of the routes 4

--- src/routers/Site.php
+++ src/routers/Site.php
@@ -4,19 +4,19 @@ class Site
     public static function sobre() 
     {
 
-        $GLOBALS['content'] =  __DIR__ . "/../views/site/sobre/sobre.php";
+        $GLOBALS['content'] =  __DIR__ . "/views/site/sobre.php";
     }
 
     public static function jogo() 
     {
 
-        $GLOBALS['content'] =  __DIR__ . "/../views/site/jogo/jogo.php";
+        $GLOBALS['content'] =  __DIR__ . "/views/site/jogo.php";
     }
 
     public static function rank() 
     {
 
-        $GLOBALS['content'] = __DIR__ . "/../views/site/rank/rank.php";
+        $GLOBALS['content'] = __DIR__ . "/views/site/rank.php";
     }
 
 }
