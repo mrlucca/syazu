9e0459c340c7d4ce667470f91f0668f78db4fc5e
mrlucca <vini.lucca42@gmail.com>
2020-11-12 20:25:13 -0300

    New implementation of the routes 2

--- src/routers/Site.php
+++ src/routers/Site.php
@@ -4,19 +4,19 @@ class Site
     public static function sobre() 
     {
 
-        $content =  __DIR__ . "/../views/site.php";
+        $content =  __DIR__ . "/../views/site/sobre/sobre.php";
     }
 
     public static function jogo() 
     {
 
-        $content =  __DIR__ . "/../views/site.php";
+        $content =  __DIR__ . "/../views/site/jogo/jogo.php";
     }
 
     public static function rank() 
     {
 
-        $content = __DIR__ . "/../views/site.php";
+        $content = __DIR__ . "/../views/site/rank/rank.php";
     }
 
 }
