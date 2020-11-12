445e032c03e963ca21c5149cfa6f70c239be90c8
mrlucca <vini.lucca42@gmail.com>
2020-11-12 19:57:19 -0300

    Test

--- main.php
+++ main.php
@@ -1,30 +1,9 @@
 <?php
 session_start();
 require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/src/routers/Routers.php';
 
 // if(!isset($_SESSION['id_usuario'])){
 //     header("location: login.php");
 //     exit;
 // }
-Flight::route('/', function(){
-    echo 'hello world!';
-});
-
-Flight::route('/teste', function(){
-    echo 'hello teste!';
-});
-
-Flight::route('/teste/hello', function(){
-    echo 'hellooooo teste!';
-});
-
-
-Flight::route('/admin', function(){
-    echo 'hello admin!';
-});
-
-Flight::route('/admin/post', function(){
-    echo 'hello admin tome post!';
-});
-
-Flight::start();
