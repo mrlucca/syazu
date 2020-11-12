a86f3a67311ca54fd102b467dee1349e9b7fcc92
mrlucca <vini.lucca42@gmail.com>
2020-11-12 19:05:47 -0300

    Test

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
 <?php
-require_once __DIR__ . "/../main.php";
+// require_once __DIR__ . "/../main.php";
+require __DIR__ . '/../vendor/autoload.php';
+
+Flight::route('/', function(){
+    echo 'hello world!';
+});
+
+Flight::route('/teste', function(){
+    echo 'hello teste!';
+});
+
+
+Flight::route('/admin', function(){
+    echo 'hello admin!';
+});
+
