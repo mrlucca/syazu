ebf386f93da47582a3ef8bc43d1d70dea5888713
mrlucca <vini.lucca42@gmail.com>
2020-11-15 13:56:58 -0300

    teste routers 2

--- src/routers/Routers.php
+++ src/routers/Routers.php
@@ -7,24 +7,24 @@ require_once __DIR__ . "/Site.php";
 // Flight::map('notFound',"Razin");
 
 Flight::route('/', function(){
-    require_once __DIR__ . "/../views/site.php";
     $content = __DIR__ . "/../site/home.php";
+    require_once __DIR__ . "/../views/site.php";
 });
 
 Flight::route('/sobre', function(){
-    require_once __DIR__ . "/../views/site.php";
     $content = __DIR__ . "/../viws/site/sobre.php";
+    require_once __DIR__ . "/../views/site.php";
 });
 
 Flight::route('/jogo', function(){
-    require_once __DIR__ . "/../views/site.php";
     $content = __DIR__ . "/../viws/site/jogo.php";
+    require_once __DIR__ . "/../views/site.php";
 });
 
 
 Flight::route('/rank', function(){
-    require_once __DIR__ . "/../views/site.php";
     $content = __DIR__ . "/../viws/site/rank.php";
+    require_once __DIR__ . "/../views/site.php";
 });
 
 Flight::start();
