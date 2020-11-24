831b948b97176b7935783cdcaa754befbb7c102b
vinilucca <vini.lucca42@gmail.com>
2020-11-24 18:25:26 -0300

    New sobre

--- src/views/site/sobre.php
+++ src/views/site/sobre.php
@@ -15,13 +15,14 @@
     <div class="container-fluid bg-2 text-center">
         <h3>O que eu vim fazer aqui? Qual o meu objetivo?</h3>
         <p>Tudo começou quando eu vim do planeta batatinhas, eu vi que aqui na terra as pessoas
-            necessitavam de alguma coisa e isso é o conhecimento...........
+            necessitavam de alguma coisa e isso é o conhecimento, então eu vim trazer para vocês um pouco do 
+            conhecimento que tenho ao logo dos milênios vagando pela terra.
         </p>
     </div>
 
     <div class="container-fluid bg-2 text-center">
         <h4>Ao longo da minha carreira de propagador do conhecimento eu fiz algumas entrevistas, fiquem a vonte para conferir!</h4>
-
+        <h3>Aproveitem o meu game e se divirtam adquirindo conhecimento!</h3>
         <div class="row justify-content-md-center">
             <div class="col-md-auto">
                 <iframe width="350" height="200" src="https://www.youtube.com/embed/Isl9xZjCBWE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
